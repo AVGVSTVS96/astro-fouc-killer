0a222ca144b693352d6ca72034aceedad3dc738e
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-07-20 16:34:43 -0400

    feat(first-working-version): Working version of astro-fouc-killer with static local storage key
    This proof of concept uses "themeToggle" as the local storage key for the dark mode setting. The local storage key cannot be set by the user.

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -1,50 +1,29 @@
-import { defineIntegration, addVirtualImports } from "astro-integration-kit";
-import { z } from "astro/zod";
+import { defineIntegration } from "astro-integration-kit";
 
 export const astroFoucKiller = defineIntegration({
   name: "astro-fouc-killer",
-  optionsSchema: z
-    .object({
-      localStorageKey: z.string(),
-    })
-    .optional(),
-  setup({ name, options = {} }) {
+  setup() {
     return {
       hooks: {
         "astro:config:setup": (params) => {
           const { injectScript } = params;
 
-          addVirtualImports(params, {
-            name,
-            imports: {
-              "virtual:astro-fouc-killer/config": `
-                export const localStorageKey = ${JSON.stringify(
-                  options?.localStorageKey ?? "themeToggle"
-                )};
-              `,
-              "virtual:astro-fouc-killer/script": `
-                import { localStorageKey } from 'virtual:astro-fouc-killer/config';
-
-                const preferredTheme =
-                  localStorage.getItem(localStorageKey) ||
-                  (window.matchMedia('(prefers-color-scheme: dark)').matches
-                    ? 'dark'
-                    : 'light');
-                document.documentElement.classList.toggle(
-                  'dark',
-                  preferredTheme === 'dark'
-                );
-                window.addEventListener('storage', () => {
-                  const isDark = localStorage.getItem(localStorageKey) === 'dark';
-                  document.documentElement.classList.toggle('dark', isDark);
-                });
-              `,
-            },
-          });
-
           injectScript(
             "head-inline",
-            `import 'virtual:astro-fouc-killer/script'`
+            `const preferredTheme =
+              localStorage.getItem("themeToggle") ||
+              (window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'dark'
+                : 'light');
+              document.documentElement.classList.toggle(
+                'dark',
+                preferredTheme === 'dark'
+              );
+              window.addEventListener('storage', () => {
+                const isDark = localStorage.getItem("themeToggle") === 'dark';
+                document.documentElement.classList.toggle('dark', isDark);
+              });
+            `
           );
         },
       },
--- package/virtual.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "virtual:astro-fouc-killer/config" {
-  export const localStorageKey: string;
-}
-
-declare module "virtual:astro-fouc-killer/script" {
-  const script: void;
-  export default script;
-}
